7f4237fbefbd2e87583cd6ec91b744989cba8940
KimmoCommit <kimmocommit@gmail.com>
2014-12-18 23:08:56 +0200

    lisätty hevosille placeholder kuva ja laitettu kursori normaaliksi jos ei kuvaa

--- js/imgError.js
+++ js/imgError.js
@@ -1,6 +1,13 @@
 function imgNotFound(image){
  	image.onerror = "";
     image.src = "media/horses/imagenotfound.jpg";
+    image.onclick = function(e){
+    	e.preventDefault();
+    	e.stopPropagation();
+    };
+    image.onmouseover = function(){
+    	$(this).css({"cursor":"default"});
+    };
     return true;
-
 }
+
