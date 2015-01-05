75ad6b75266a774bfa1acfcd76cc680f14966c20
KimmoCommit <kimmocommit@gmail.com>
2015-01-05 16:48:22 +0200

    uudet kuvat kisoille ja kursseille, leirikuvaukset angularilla

--- js/controllers/leiritController.js
+++ js/controllers/leiritController.js
@@ -2,10 +2,22 @@ var app = angular.module("elementApp");
 
 app.controller('leiritController', ['$scope','$http', function($scope, $http){
 	$scope.Leirit = '';
+
 	$http.get('res/leirit.json').
 	then(function(res){
 		$scope.Leirit = res.data;
 	});
+
+	$scope.Leirikuvaukset = '';
+
+	$http.get('res/leirikuvaukset.json').
+	then(function(res){
+		$scope.Leirikuvaukset = res.data;
+	});
+
+
+
+
 }]);
 
 
