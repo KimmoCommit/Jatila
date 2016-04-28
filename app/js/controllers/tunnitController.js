dd6717ede67d33e67e0fa61d8c36bab16b0a33cb
KimmoCommit <kimmocommit@gmail.com>
2016-04-28 22:24:40 +0300

    added toggle for Tunnit and a description data, added max-width for hinnat

--- app/js/controllers/tunnitController.js
+++ app/js/controllers/tunnitController.js
@@ -1,6 +1,6 @@
 var app = angular.module("elementApp");
 
-app.controller('tunnitController', ['$scope','$http', function($scope, $http){
+app.controller('tunnitController', ['$scope','$http','$sce', function($scope, $http, $sce){
 	$(".fb-page").hide();
 	$scope.Tunnit = '';
 	$scope.Tuntikuvaukset = '';
@@ -8,8 +8,11 @@ app.controller('tunnitController', ['$scope','$http', function($scope, $http){
 
 	$http.get('res/tunnit.json').
 	then(function(res){
+		var KausiData = res.data[1][0] 
+		$scope.Kausi = KausiData.season;
+		$scope.KausiKuvaus = $sce.trustAsHtml(KausiData.description);
+		$scope.VainKuvaus = KausiData.onlyDescription;
 		$scope.Tunnit = res.data[0];
-		$scope.Kausi = res.data[1][0].season;
 	});
 	$http.get('res/tuntikuvaukset.json').
 	then(function(res){
